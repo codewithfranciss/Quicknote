0a4105d4cb985b533603ac9440aff25dfe1fb9ac
codewithfrancis <offor919@gmail.com>
2024-08-24 21:40:56 +0100

    Create Notes

--- quicknotes/lib/supabaseClient.js
+++ quicknotes/lib/supabaseClient.js
@@ -1,5 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey)
+// Replace these with your actual Supabase URL and Anon Key
+const supabaseUrl = 'https://xncsqbtejxosndzlfnwo.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhuY3NxYnRlanhvc25kemxmbndvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQxNTczODYsImV4cCI6MjAzOTczMzM4Nn0.min2-b0WUWayCEe4SOv5tHW31eLWQNlTztq8lZy9fLE';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
--- quicknotes/src/components/CreateNotes.jsx
+++ quicknotes/src/components/CreateNotes.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import {supabase} from '../../lib/supabaseClient'
+const NotesForm = () => {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
 
-const Create = () => {
-  return (
-    <div className=''>createNotes</div>
-  )
-}
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if(!title=='' || description==''){
+      alert('Please fill in all fields')
+    }else{
+    const {error} = await supabase
+    .from('notes')
+    .insert({
+      title: title,
+      description: description
+    })
 
-export default Create
+
+    if(!error){
+      alert("Notes Created Successfully")
+      setDescription('')
+      setTitle('')
+    }else
+    console.log(error)
+
+  }}
+    return (
+   
+        <div className="flex justify-center mt-10">
+          
+            <div className="w-full md:w-3/4 lg:w-1/2">
+            <form action="" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    className="w-full dark:bg-base-100 p-4 mb-4 text-lg border border-gray-300 rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    placeholder="Title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <textarea
+                    className="w-full dark:bg-base-100 h-64 p-4 text-lg border border-gray-300 rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    placeholder="Description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+                 <button className="btn dark:bg-base-100 mt-2 mb-6 border-gray-300   btn-outline w-full">Create Note</button>
+                 </form>
+            </div>
+           
+        </div>
+       
+    );
+};
+
+export default NotesForm;
